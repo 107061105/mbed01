8e3a3bc67adb6c5cca48cecd820e167a9d943b07
107061105 <yc95086@gmail.com>
2021-02-25 04:24:49 -0800

    add led.cpp

--- demo/led.cpp
+++ demo/led.cpp
@@ -1,10 +1,10 @@
 #include "mbed.h"
 
-void Led(DigitalOut &ledpin, n)
+void Led(DigitalOut &ledpin, int n)
 {
    for (int i = 0; i < 2 * n; ++i)
    {                     //blink for n times
       ledpin = !ledpin; // toggle led
-      ThisThread::sleep_for(100ms);
+      ThisThread::sleep_for(1s);
    }
 }
